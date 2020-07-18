43af148d34086472fa71ad931fefcd9de84f4ab3
JulienLeotier <julien.leotier@gmail.com>
2020-07-18 12:03:48 +0200

    wip

--- app/screens/connexion-screen/connexion-screen.tsx
+++ app/screens/connexion-screen/connexion-screen.tsx
@@ -1,36 +1,104 @@
 import React, { FunctionComponent as Component } from "react"
-import { View, SafeAreaView } from "react-native"
+import { View, ViewStyle, TextStyle, SafeAreaView } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { observer } from "mobx-react-lite"
 import { Button, Header, Screen, Text, Wallpaper } from "../../components"
-import { color } from "../../theme"
+import { color, spacing, typography } from "../../theme"
 
+
+const FULL: ViewStyle = { flex: 1 }
+const CONTAINER: ViewStyle = {
+    backgroundColor: color.transparent,
+    paddingHorizontal: spacing[4],
+}
+const TEXT: TextStyle = {
+    color: color.palette.white,
+    fontFamily: typography.primary,
+}
+const BOLD: TextStyle = { fontWeight: "bold" }
+const HEADER: TextStyle = {
+    paddingTop: spacing[3],
+    paddingBottom: spacing[4] + spacing[1],
+    paddingHorizontal: 0,
+}
+const HEADER_TITLE: TextStyle = {
+    ...TEXT,
+    ...BOLD,
+    fontSize: 12,
+    lineHeight: 15,
+    textAlign: "center",
+    letterSpacing: 1.5,
+}
+const TITLE_WRAPPER: TextStyle = {
+    ...TEXT,
+    textAlign: "center",
+}
+const TITLE: TextStyle = {
+    ...TEXT,
+    ...BOLD,
+    fontSize: 28,
+    lineHeight: 38,
+    textAlign: "center",
+}
+const ALMOST: TextStyle = {
+    ...TEXT,
+    ...BOLD,
+    fontSize: 26,
+    fontStyle: "italic",
+}
+
+const CONTENT: TextStyle = {
+    ...TEXT,
+    color: "#BAB6C8",
+    fontSize: 15,
+    lineHeight: 22,
+    marginBottom: spacing[5],
+}
+const CONTINUE: ViewStyle = {
+    paddingVertical: spacing[4],
+    paddingHorizontal: spacing[4],
+    backgroundColor: "#5D2555",
+}
+const CONTINUE_TEXT: TextStyle = {
+    ...TEXT,
+    ...BOLD,
+    fontSize: 13,
+    letterSpacing: 2,
+}
+const FOOTER: ViewStyle = { backgroundColor: "#20162D" }
+const FOOTER_CONTENT: ViewStyle = {
+    paddingVertical: spacing[4],
+    paddingHorizontal: spacing[4],
+}
 export const ConnexionScreen: Component = observer(function ConnexionScreen() {
     const navigation = useNavigation()
-    const nextScreen = () => navigation.navigate("welcom")
+    const nextScreen = () => navigation.navigate("welcome")
 
     return (
-        <View>
+        <View style={FULL}>
             <Wallpaper />
-            <Screen preset="scroll" backgroundColor={color.transparent}>
-                <Header headerTx="ConnexionScreen.poweredBy" />
-                <Text>
-                    <Text text="Connexion, " />
-                    <Text text="almost" />
-                    <Text text="!" />
+            <Screen style={CONTAINER} preset="scroll" backgroundColor={color.transparent}>
+                <Header headerTx="ConnexionScreen.poweredBy" style={HEADER} titleStyle={HEADER_TITLE} />
+                <Text style={TITLE_WRAPPER}>
+                    <Text style={TITLE} text="my app" />
+                    <Text style={ALMOST} text="almost" />
+                    <Text style={TITLE} text="!" />
                 </Text>
-                <Text preset="header" tx="ConnexionScreen.readyForLaunch" />
-                <Text>
-                    I'm a connexion screen
+                <Text style={TITLE} preset="header" tx="ConnexionScreen.readyForLaunch" />
+                <Text style={CONTENT}>
+                    This probably isn't what your app is going to look like. Unless your designer handed you
+                    this screen and, in that case, congrats! You're ready to ship.
           </Text>
-                <Text>
+                <Text style={CONTENT}>
                     For everyone else, this is where you'll see a live preview of your fully functioning app
                     using Ignite.
           </Text>
             </Screen>
-            <SafeAreaView>
-                <View >
+            <SafeAreaView style={FOOTER}>
+                <View style={FOOTER_CONTENT}>
                     <Button
+                        style={CONTINUE}
+                        textStyle={CONTINUE_TEXT}
                         tx="ConnexionScreen.continue"
                         onPress={nextScreen}
                     />
--- app/theme/color.ts
+++ app/theme/color.ts
@@ -20,6 +20,7 @@ export const color = {
    * by their under-powered GPUs.
    */
   transparent: "rgba(0, 0, 0, 0)",
+
   /**
    * The screen background.
    */
